2f6e203aeb14b27e3d6cabfd5049a434ecdae01b
cwieder <cwieder09@gmail.com>
2020-11-09 11:12:47 +0000

    KEGG database downloader

---
+++ download_KEGG.py
@@ -0,0 +1,42 @@
+# Get latest KEGG pathways using rest API
+
+import requests
+import re
+import pandas as pd
+
+# get all pathways
+url = 'http://rest.kegg.jp/list/pathway'
+data = requests.get(url)
+pathways = data.text
+pathways = pathways.split("\n")
+pathways = filter(None, pathways)
+pathway_dict = dict()
+
+for path in pathways:
+    path = path.split("\t")
+    name = path[1]
+    pathid = re.search(r"path:(.*)", path[0]).group(1)
+    pathway_dict[pathid] = name
+
+# get compounds for each pathway
+base_url = 'http://rest.kegg.jp/link/cpd/'
+pathway_ids = [*pathway_dict]
+
+pathway_compound_mapping = dict()
+for i in pathway_ids:
+    current_url = base_url + i
+    compounds = requests.get(current_url)
+    c_list = compounds.text.split("\n")
+    c_list = filter(None, c_list)
+    complist = []
+    for comp in c_list:
+        comp = comp.split("\t")
+        complist.append(comp[1])
+    pathway_compound_mapping[i] = complist
+
+print(pathway_compound_mapping)
+df = pd.DataFrame.from_dict(pathway_compound_mapping, orient='index')
+df.to_csv("KEGG_reference_pathways_compounds.csv")
+
+
+
