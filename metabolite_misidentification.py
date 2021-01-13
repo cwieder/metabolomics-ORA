fix heatmap calculation add vary p-cutoff script

metabolite_misidentification.py
@@ -79,7 +79,7 @@ def random_misidentification(db="KEGG"):
                 results_lists.append([d, i] + res[:-1])
 
     res_df = pd.DataFrame(results_lists, columns=["Dataset", "Percentage misidentification", "n_p_less_0.1", "n_q_less_0.1", "p_std", "q_std"])
-    res_df.to_csv("Metabolite_misidentification_simulation_random_Reactome.csv")
+    # res_df.to_csv("Metabolite_misidentification_simulation_random_Reactome.csv")
 
     simulation_res = res_df
     plt.figure()
@@ -93,7 +93,7 @@ def random_misidentification(db="KEGG"):
     plt.legend()
     plt.ylabel("Mean number of pathways significant at P < 0.1 \n based on 100 random permutations")
     plt.xlabel("Percentage of metabolites misidentified")
-    plt.savefig("metabolite_random_misidentification_Reactome.png", dpi=300)
+    plt.savefig("metabolite_random_misidentification_Reactome2.png", dpi=300)
     plt.show()
 
 # random_misidentification(db="Reactome")
@@ -133,7 +133,7 @@ def TPR_heatmap(pg, fname, db="KEGG"):
                     number_common_paths = len([i for i in x if i in original_pathways])
                     fraction_pathways_TPR = number_common_paths/total_significant_paths
                     pathway_fractions_TPR.append(fraction_pathways_TPR)
-                    fraction_pathways_FPR = (total_significant_paths-number_common_paths)/total_significant_paths
+                    fraction_pathways_FPR = len([i for i in x if i not in original_pathways])/total_significant_paths
                     pathway_fractions_FPR.append(fraction_pathways_FPR)
                 avg_fraction_TPR = np.mean(pathway_fractions_TPR)
                 avg_fraction_FPR = np.mean(pathway_fractions_FPR)
@@ -153,7 +153,7 @@ def TPR_heatmap(pg, fname, db="KEGG"):
                     number_common_paths = len([i for i in x if i in original_pathways])
                     fraction_pathways_TPR = number_common_paths/total_significant_paths
                     pathway_fractions_TPR.append(fraction_pathways_TPR)
-                    fraction_pathways_FPR = (total_significant_paths-number_common_paths)/total_significant_paths
+                    fraction_pathways_FPR = len([i for i in x if i not in original_pathways])/total_significant_paths
                     pathway_fractions_FPR.append(fraction_pathways_FPR)
                 avg_fraction_TPR = np.mean(pathway_fractions_TPR)
                 avg_fraction_FPR = np.mean(pathway_fractions_FPR)
@@ -174,13 +174,16 @@ def TPR_heatmap(pg, fname, db="KEGG"):
     sns.heatmap(res_df_TPR, annot=True, cmap="mako", square=True)
     plt.subplot(122)
     plt.title('FPR')
-    sns.heatmap(res_df_FPR, annot=True, cmap="rocket", square=True)
+    sns.heatmap(res_df_FPR, annot=True, cmap="rocket_r", square=True)
     plt.subplots_adjust(bottom=0.28)
         # plt.ylabel("Percentage metabolite misidentification (%)")
     plt.savefig(fname, dpi=300)
     plt.show()
 
-TPR_heatmap(param_grid_heatmaps["mass"], "mass_misidentification_heatmap_Reactome.png", db="Reactome")
+# TPR_heatmap(param_grid_heatmaps["random"], "formula_misidentification_heatmap_Reactome_new.png", db="Reactome")
+TPR_heatmap(param_grid_heatmaps["formula"], "formula_misidentification_heatmap_Reactome_new.png", db="Reactome")
+TPR_heatmap(param_grid_heatmaps["mass"], "mass_misidentification_heatmap_Reactome_new.png", db="Reactome")
+
 
 
 def misidentification_mass_plot(db="KEGG"):
@@ -225,7 +228,7 @@ def misidentification_mass_plot(db="KEGG"):
     # plt.savefig("metabolite_misidentification_by_mass_Reactome.png", dpi=300)
     plt.show()
 
-misidentification_mass_plot(db="KEGG")
+# misidentification_mass_plot(db="KEGG")
 
 def misidentification_formula_plot(db="KEGG"):
     d_sets = datasets

utils.py
@@ -237,7 +237,7 @@ def misidentify_metabolites(percentage, processed_matrix, organism_compounds, ba
     p_vals = []
     q_vals = []
     significant_pathways = []  # at P < 0.1
-
+    print(str(len(organism_compounds)), "Reactome organism compounds")
     if zamboni == False:
         mat_unannotated = processed_matrix.iloc[:, :-1]
         metabolites = mat_unannotated.columns.tolist()
@@ -248,6 +248,7 @@ def misidentify_metabolites(percentage, processed_matrix, organism_compounds, ba
             replacement_compounds = np.random.choice(np.setdiff1d(organism_compounds, background_list), n_misidentified, replace=False)
             # TODO: CHECK THAT ORGANISM COMPOUNDS CONTAIN THE RIGHT THINGS e.g. glycans? drugs? should be included?
             replacement_dict = dict(zip(metabolites_to_replace, replacement_compounds))
+            print(replacement_dict)
             misidentified_matrix = mat_unannotated.rename(columns=replacement_dict)
 
             # Perform t-tests and ORA
@@ -267,6 +268,7 @@ def misidentify_metabolites(percentage, processed_matrix, organism_compounds, ba
                                                                   [background_list + list(metabolites_to_replace)]), n_misidentified,
                                                      replace=False)
             replacement_dict = dict(zip(metabolites_to_replace, replacement_compounds))
+            print(replacement_dict)
             misidentified_matrix = processed_matrix.rename(columns=replacement_dict)
             DEM = []
             for x in misidentified_matrix.T.itertuples():
@@ -322,7 +324,7 @@ def misidentify_metabolites_by_mass(percentage, processed_matrix, pathway_df, al
             if len(cpd_info) > 1:
                 misidentifiable_metabolites[cpd] = np.setdiff1d(cpd_info, cpd).tolist()
         print(len(misidentifiable_metabolites))
-        for i in range(0, 1):
+        for i in range(0, 100):
             replacement_dict = dict()
             while len(replacement_dict) < n_misidentified:
                 cpd_to_relpace = np.random.choice(list(misidentifiable_metabolites.keys()), 1)[0]
@@ -355,7 +357,7 @@ def misidentify_metabolites_by_mass(percentage, processed_matrix, pathway_df, al
                 misidentifiable_metabolites[cpd] = np.setdiff1d(cpd_info, cpd).tolist()
 
         print(len(misidentifiable_metabolites))
-        for i in range(0, 1):
+        for i in range(0, 100):
             replacement_dict = dict()
             while len(replacement_dict) < n_misidentified:
                 cpd_to_relpace = np.random.choice(list(misidentifiable_metabolites.keys()), 1)[0]