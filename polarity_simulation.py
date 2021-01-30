5dc6dd1809f3f2630d8454aaf0f05fc65ea87d53
cwieder <cwieder09@gmail.com>
2021-01-30 14:07:54 +0000

    added polarity simulation

--- polarity_simulation.py
+++ polarity_simulation.py
@@ -3,7 +3,8 @@ import pandas as pd
 import utils
 import process_datasets
 import matplotlib.pyplot as plt
-
+from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
+import numpy as np
 logp_all = pd.read_csv("hmdb_logp_all.csv", index_col=0)
 print(logp_all.shape)
 
@@ -31,7 +32,7 @@ matching_id = logp_all[logp_all["kegg_id"].isin(cols)]
 # plt.show()
 
 # Brown - split at -1 or -2
-cutoff = -1
+cutoff = -2
 polar = matching_id[matching_id["logp"] < cutoff]
 nonpolar = matching_id[matching_id["logp"] > cutoff]
 
@@ -39,14 +40,42 @@ nonpolar = matching_id[matching_id["logp"] > cutoff]
 
 polar_mat = mat_brown.filter(polar["kegg_id"])
 polar_mat = polar_mat.iloc[:, ~polar_mat.columns.duplicated()]
-
 ttest_polar = utils.t_tests(polar_mat, mat_brown["Group"], "fdr_bh")
 dem_polar = ttest_polar[ttest_polar["P-adjust"] <= 0.05]["Metabolite"].tolist()
-
 ora_polar = utils.over_representation_analysis(dem_polar, polar_mat.columns.tolist(), KEGG_mouse_pathways)
+
 nonpolar_mat = mat_brown.filter(nonpolar["kegg_id"])
+nonpolar_mat = nonpolar_mat.iloc[:, ~nonpolar_mat.columns.duplicated()]
+ttest_nonpolar = utils.t_tests(nonpolar_mat, mat_brown["Group"], "fdr_bh")
+dem_nonpolar = ttest_nonpolar[ttest_nonpolar["P-adjust"] <= 0.05]["Metabolite"].tolist()
+ora_nonpolar = utils.over_representation_analysis(dem_nonpolar, nonpolar_mat.columns.tolist(), KEGG_mouse_pathways)
 
 print(ora_polar[ora_polar["P-value"] <= 0.1])
+print(ora_nonpolar[ora_nonpolar["P-value"] <= 0.1])
 
+polar_pathways = ora_polar[ora_polar["P-value"] <= 0.1]["Pathway_ID"].tolist()
+nonpolar_pathways = ora_nonpolar[ora_nonpolar["P-value"] <= 0.1]["Pathway_ID"].tolist()
+common_paths = list(set(polar_pathways) & set(nonpolar_pathways))
+
+pp_names = [' '.join(i.split(' ')[:-4]) for i in ora_polar[ora_polar["P-value"] <= 0.1]["Pathway_name"].tolist()]
+np_names = [' '.join(i.split(' ')[:-4]) for i in ora_nonpolar[ora_nonpolar["P-value"] <= 0.1]["Pathway_name"].tolist()]
+common_names = list(set(pp_names) & set(np_names))
+
+plt.style.use("seaborn")
+venn = venn2(subsets = (set(polar_pathways),
+                        set(nonpolar_pathways)), set_labels = ('Polar compounds', 'Non-polar compounds'),
+             set_colors=('tab:blue', 'tab:orange'), alpha = 0.5)
+venn.get_label_by_id('100').set_text("\n".join([i for i in pp_names if i not in common_names]))
+venn.get_label_by_id('110').set_text("\n".join(common_names))
+venn.get_label_by_id('010').set_text("\n".join([i for i in np_names if i not in common_names]))
+venn2_circles(subsets = (set(polar_pathways),
+                        set(nonpolar_pathways)),
+              linestyle='solid', linewidth=0.5, color='k')
+plt.annotate('Unknown set', xy=venn.get_label_by_id('110').get_position() - np.array([0, 0.05]), xytext=(-70,-70),
+ha='center', textcoords='offset points', bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
+arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5',color='gray'))
+
+plt.tight_layout()
+plt.show()
 
 
